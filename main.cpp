31b3d0c8b26e2f769acb2e1f41364dcc656feed2
FxxFighter <xofighter24@gmail.com>
2025-03-17 02:10:58 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Student {
+    string name;
+    double grades[4]; // دقیقاً 4 نمره برای هر دانشجو
+    double average;
+};
+
+// تابع محاسبه معدل
+double calculateAverage(double grades[], int numGrades) {
+    double sum = 0;
+    for (int i = 0; i < numGrades; i++) {
+        sum += grades[i];
+    }
+    return sum / numGrades;
+}
+
+int main() {
+    ifstream inputFile("students.txt");
+    if (!inputFile) {
+        cerr << "خطا در باز کردن فایل!" << endl;
+        return 1;
+    }
+
+    Student students[1000]; // حداکثر 1000 دانشجو
+    int studentCount = 0;
+    double totalAverage = 0.0;
+
+    while (studentCount < 1000 && inputFile >> students[studentCount].name) {
+        for (int i = 0; i < 4; i++) {
+            if (!(inputFile >> students[studentCount].grades[i])) {
+                cerr << "خطا در خواندن نمرات!" << endl;
+                return 1;
+            }
+        }
+
+        students[studentCount].average = calculateAverage(students[studentCount].grades, 4);
+        totalAverage += students[studentCount].average;
+        studentCount++;
+    }
+
+    inputFile.close();
+
+    // نمایش نتایج
+    for (int i = 0; i < studentCount; i++) {
+        cout << "نام: " << students[i].name << ", معدل: " << students[i].average << endl;
+    }
+
+    if (studentCount > 0) {
+        totalAverage /= studentCount;
+        cout << "\nمعدل کل دانشجویان: " << totalAverage << endl;
+    }
+
+    return 0;
+}
